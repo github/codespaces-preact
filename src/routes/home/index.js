22629b5880dbb2853e00908f11badd3acc3b6408
Brian McManus <bdmac@github.com>
2022-10-24 19:29:38 +0000

    Tweak the default app

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,7 +3,8 @@ import style from './style.css';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
+		<h1>GitHub Codespaces <span class={style.heart}>♥️</span> Preact</h1>
+		<img src="/assets/Octocat.png" alt="Mona" class={style.octocat} />
 		<p>This is the Home component.</p>
 	</div>
 );
